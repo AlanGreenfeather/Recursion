ad1838865b46ec2e1b7484ed2e58a00ba417f4ca
{Alan <{alan.j.greenfeather-1@ou.edu}>
2020-11-09 16:30:39 -0600

    Ate Cake

--- src/Cake.java
+++ src/Cake.java
@@ -1,4 +1,16 @@
 
 public class Cake {
-
+	private int bites = 10;
+	public Cake() {
+		// nothing to implement
+	}
+	
+	public void takeABite() {
+		bites--;
+		System.out.println("Yum");
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+	}
 }
