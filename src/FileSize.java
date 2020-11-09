7b1cfa5039e45a3e75a8e1769fad2fdc4abe8df8
{Alan <{alan.j.greenfeather-1@ou.edu}>
2020-11-09 17:01:28 -0600

    Files sized

---
+++ src/FileSize.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class FileSize {
+	/*
+	 * Directoy
+	 * |
+	 * | file1 file2 file3 dir1
+	 * 						|
+	 * 						file4 file 5 dir2
+	 * 										|
+	 * 										|
+	 * 										file6
+	 * 
+	 */
+	
+	public static long getSize(File file){
+		long size = 0;
+		// if file => get size
+		// if dir => get all of the files and
+		// process all of the files and dir
+		if( file.isDirectory()) {
+			File[] files = file.listFiles();
+			// for every of these files
+			List<File> xFiles = new LinkedList<>(Arrays.asList(files));
+			for (File f: xFiles) {
+				size += getSize(f);
+			}
+			
+		} else {
+			size += file.length();
+		}
+				
+		return size;
+	}
+	public static void main(String args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please give me a director or a file");
+		String str = input.nextLine();
+		System.out.println("The size is: " 
+		+ getSize());
+	}
+}
